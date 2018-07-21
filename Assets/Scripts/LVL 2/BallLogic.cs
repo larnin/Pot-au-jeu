1de64cfb08d00517fbd846708678e10b99d2e61b
Nicolas <nicolas-donald.laurent@laposte.net>
2018-07-21 22:19:50 +0200

    "ball" system

---
+++ Assets/Scripts/LVL 2/BallLogic.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using NRand;
+using System.Collections.Generic;
+
+public class BallLogic : MonoBehaviour
+{
+    [SerializeField] int m_id;
+    [SerializeField] Color m_color;
+    [SerializeField] float m_effectTime;
+    [SerializeField] float m_speed;
+    
+    public int id { get { return m_id; } }
+    public float effectTime { get { return m_effectTime; } }
+    public Color color { get { return m_color; } }
+
+    RectInt m_path;
+
+    public void setPath(RectInt path)
+    {
+        m_path = path;
+        StartCoroutine(run());
+    }
+
+    IEnumerator run()
+    {
+        List<Vector2Int> points = new List<Vector2Int> {new Vector2Int(m_path.min.x, m_path.max.y), m_path.min, new Vector2Int(m_path.max.x, m_path.min.y), m_path.max};
+        bool direction = new BernoulliDistribution().Next(new StaticRandomGenerator<DefaultRandomGenerator>());
+        if(direction)
+        {
+            var temp = points[0];
+            points[0] = points[3];
+            points[3] = temp;
+            temp = points[2];
+            points[2] = points[1];
+            points[1] = temp;
+        }
+
+        int currentIndex = 0;
+
+        while (true)
+        {
+            var d = (new Vector2(transform.position.x, transform.position.y) - points[currentIndex]).magnitude;
+
+            transform.DOMove(new Vector3(points[currentIndex].x, points[currentIndex].y, transform.position.z), d / m_speed).SetEase(Ease.Linear);
+
+            currentIndex = (currentIndex + 1) % points.Count;
+            yield return new WaitForSeconds(d / m_speed);
+        }
+    }
+}
---
+++ Assets/Scripts/LVL 2/SpawnerLogic.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using NRand;
+
+public class SpawnerLogic : StartableLogic
+{
+    [SerializeField] GameObject m_redBall;
+    [SerializeField] GameObject m_pinkBall;
+    [SerializeField] GameObject m_blueBall;
+    [SerializeField] GameObject m_greenBall;
+    [SerializeField] int m_range = 5;
+    [SerializeField] float m_redProbability = 0.5f;
+    [SerializeField] float m_spawnTime = 2;
+    [SerializeField] float m_spawnDelay = 0.5f;
+    [SerializeField] float m_spawnHeight = 8;
+    [SerializeField] float m_moveSpeed = 1;
+    [SerializeField] List<RectInt> m_possiblePath; 
+
+    float m_timer = 0;
+    bool m_spawning = false;
+
+    bool m_direction = true;
+    float m_centerPos;
+
+    GameObject m_spawnedPink = null;
+    GameObject m_spawnedBlue = null;
+    GameObject m_spawnedGreen = null;
+
+    protected override void onAwake()
+    {
+        m_centerPos = transform.position.x;   
+    }
+
+    protected override void onUpdate()
+    {
+        m_timer -= Time.deltaTime;
+        if (m_timer <= 0 && !m_spawning)
+            spawn();
+
+        if (!m_spawning)
+            move();
+    }
+
+    void spawn()
+    {
+        GameObject objectToSpawn = m_redBall;
+        if (m_spawnedPink == null)
+            objectToSpawn = m_pinkBall;
+        if (m_spawnedGreen == null)
+            objectToSpawn = m_greenBall;
+        if (m_spawnedBlue == null)
+            objectToSpawn = m_blueBall;
+
+        bool toRed = new BernoulliDistribution(m_redProbability).Next(new StaticRandomGenerator<DefaultRandomGenerator>());
+
+        if (objectToSpawn == m_redBall && !toRed)
+        {
+            m_timer = m_spawnTime;
+            return;
+        }
+        if (objectToSpawn != m_redBall && toRed)
+            objectToSpawn = m_redBall;
+
+        m_spawning = true;
+        DOVirtual.DelayedCall(m_spawnDelay, () =>
+        {
+            var obj = Instantiate(objectToSpawn, new Vector3(transform.position.x, m_spawnHeight, -0.5f), Quaternion.identity);
+            var comp = obj.GetComponent<BallLogic>();
+            if (comp != null)
+                comp.setPath(m_possiblePath[new UniformIntDistribution(m_possiblePath.Count - 1).Next(new StaticRandomGenerator<DefaultRandomGenerator>())]);
+
+            if (objectToSpawn == m_pinkBall)
+                m_spawnedPink = obj;
+            if (objectToSpawn == m_greenBall)
+                m_spawnedGreen = obj;
+            if (objectToSpawn == m_blueBall)
+                m_spawnedBlue = obj;
+
+            m_timer = m_spawnTime;
+            DOVirtual.DelayedCall(m_spawnDelay, () => m_spawning = false);
+        });
+    }
+
+    void move()
+    {
+        var pos = transform.position.x - m_centerPos;
+        if (pos < -m_range)
+            m_direction = true;
+        if (pos > m_range)
+            m_direction = false;
+
+        float move = Time.deltaTime * m_moveSpeed * (m_direction ? 1 : -1);
+        transform.position = new Vector3(transform.position.x + move, transform.position.y, transform.position.z);
+    }
+}
