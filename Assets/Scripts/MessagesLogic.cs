dd34388c48d1c2646eecb9553764a531261f5858
Nicolas <nicolas-donald.laurent@laposte.net>
2018-07-22 02:06:59 +0200

    Start/end level text

---
+++ Assets/Scripts/MessagesLogic.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class MessagesLogic : MonoBehaviour
+{
+    [SerializeField] GameObject m_startMessage;
+    [SerializeField] GameObject m_endWinMessage;
+    [SerializeField] GameObject m_dieMessage;
+    [SerializeField] float m_startMessageTime = 1.0f;
+    [SerializeField] float m_timeBeforeEndMessage = 1.0f;
+    [SerializeField] float m_timeBeforeBackLobby = 3.0f;
+    [SerializeField] string m_lobbyName = "Lobby";
+
+    SubscriberList m_subscriberList = new SubscriberList();
+
+    private void Awake()
+    {
+        m_subscriberList.Add(new Event<StartEvent>.Subscriber(onStart));
+        m_subscriberList.Add(new Event<DieEvent>.Subscriber(onDie));
+        m_subscriberList.Add(new Event<BossDieEvent>.Subscriber(onBossDieEvent));
+        m_subscriberList.Subscribe();
+
+        m_startMessage.SetActive(false);
+        m_endWinMessage.SetActive(false);
+        m_dieMessage.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        m_subscriberList.Unsubscribe();
+    }
+
+    void onStart(StartEvent e)
+    {
+        m_startMessage.SetActive(true);
+        DOVirtual.DelayedCall(m_startMessageTime, () => m_startMessage.SetActive(false));
+    }
+
+    void onDie(DieEvent e)
+    {
+        DOVirtual.DelayedCall(m_timeBeforeEndMessage, () => m_dieMessage.SetActive(true));
+        DOVirtual.DelayedCall(m_timeBeforeBackLobby, () => SceneSystem.changeScene(m_lobbyName));
+        States.instance.deaths++;
+    }
+
+    void onBossDieEvent(BossDieEvent e)
+    {
+        DOVirtual.DelayedCall(m_timeBeforeEndMessage, () => m_endWinMessage.SetActive(true));
+        DOVirtual.DelayedCall(m_timeBeforeBackLobby, () => SceneSystem.changeScene(m_lobbyName));
+
+        States.instance.setFiniedScene(SceneManager.GetActiveScene().name);
+    }
+}
+
---
+++ Assets/Scripts/States.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class States
+{
+    static States m_instance;
+
+    public static States instance
+    {
+        get
+        {
+            if (m_instance == null)
+                m_instance = new States();
+            return m_instance;
+        }
+    }
+
+    public float time { get; set; }
+    public int deaths { get; set; }
+    public int plays { get; set; }
+
+    Dictionary<string, bool> m_finishedScenes;
+    public void setFiniedScene(string name)
+    {
+        if(!isFinishedScene(name))
+            m_finishedScenes.Add(name, true);
+    }
+
+    public bool isFinishedScene(string name)
+    {
+        return m_finishedScenes.ContainsKey(name);
+    }
+
+    public void reset()
+    {
+        time = 0;
+        deaths = 0;
+        plays = 0;
+        m_finishedScenes.Clear();
+    }
+}
